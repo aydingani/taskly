d3ded49f13b97fbc77622bff94ddcfb11358756c
Aydin Gani <73241817+aydingani@users.noreply.github.com>
2024-08-26 21:56:56 +0400

    adding a delete button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,33 @@
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, TouchableOpacity, Alert } from 'react-native'
 import { theme } from '@/theme'
 
 export default function HomeScreen() {
+    const handleDelete = () => {
+        Alert.alert(
+            'Are you sure you want to delete this?',
+            'It will be gone for good',
+            [
+                {
+                    text: 'Yes',
+                    onPress: () => console.log('Ok, deleting.'),
+                    style: 'destructive',
+                },
+                { text: 'Cancel', style: 'cancel' },
+            ]
+        )
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.itemContainer}>
                 <Text style={styles.itemText}>Coffee</Text>
+                <TouchableOpacity
+                    onPress={handleDelete}
+                    style={styles.button}
+                    activeOpacity={0.8}
+                >
+                    <Text style={styles.buttonText}>Delete</Text>
+                </TouchableOpacity>
             </View>
         </View>
     )
@@ -23,9 +45,23 @@ const styles = StyleSheet.create({
         paddingHorizontal: 8,
         borderBottomColor: theme.colorCerulan,
         borderBottomWidth: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
     },
     itemText: {
         fontSize: 18,
         fontWeight: '200',
+    },
+    button: {
+        backgroundColor: theme.colorBlack,
+        padding: 8,
+        borderRadius: 6,
+    },
+    buttonText: {
+        color: theme.colorWhite,
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        letterSpacing: 1,
     },
 })
--- theme.ts
+++ theme.ts
@@ -1,4 +1,5 @@
 export const theme = {
     colorCerulan: '#1a759f',
     colorWhite: '#fff',
+    colorBlack: '#000',
 }
