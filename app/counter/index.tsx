b216829cd6356c4789675bf7ef1f4f9e76fa5b52
Aydin Gani <73241817+aydingani@users.noreply.github.com>
2024-09-05 20:41:27 +0400

    adding local notifications nad countdown state persistance

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,9 +1,146 @@
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, TouchableOpacity, Alert } from 'react-native'
+import { useEffect, useState } from 'react'
+import { intervalToDuration, isBefore } from 'date-fns'
+import { TimeSegment } from '@/components/TimeSegment'
+import { theme } from '@/theme'
+import { registerForPushNotificationsAsync } from '@/utils/registerForPushNotificationsAsync'
+import * as Notifications from 'expo-notifications'
+import { getFromStorage, saveToStorage } from '@/utils/storage'
+
+const frequency = 10 * 1000
+
+const countdownStorageKey = 'taskly-countdown'
+
+type PersistedCountdownState = {
+    currentNotificationId: string | undefined
+    completedAtTimestamps: number[]
+}
+
+type CountdownStatus = {
+    isOverdue: boolean
+    distance: ReturnType<typeof intervalToDuration>
+}
 
 export default function CounterScreen() {
+    const [countdownState, setCountdownState] =
+        useState<PersistedCountdownState>()
+
+    const [status, setStatus] = useState<CountdownStatus>({
+        isOverdue: false,
+        distance: {},
+    })
+
+    useEffect(() => {
+        const init = async () => {
+            const value = await getFromStorage(countdownStorageKey)
+            setCountdownState(value)
+        }
+        init()
+    }, [])
+
+    const lastCompletedAt = countdownState?.completedAtTimestamps[0]
+
+    const scheduleNotification = async () => {
+        let pushNotificationId
+        const result = await registerForPushNotificationsAsync()
+        if (result === 'granted') {
+            pushNotificationId = await Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'The thing is due!',
+                },
+                trigger: {
+                    seconds: frequency / 1000,
+                },
+            })
+        } else {
+            Alert.alert(
+                'Unable to schedule notification',
+                'Enable the notifications permission for Expo Go in settings'
+            )
+        }
+
+        if (countdownState?.currentNotificationId) {
+            await Notifications.cancelScheduledNotificationAsync(
+                countdownState.currentNotificationId
+            )
+        }
+
+        const newCountdownState: PersistedCountdownState = {
+            currentNotificationId: pushNotificationId,
+            completedAtTimestamps: countdownState
+                ? [Date.now(), ...countdownState.completedAtTimestamps]
+                : [Date.now()],
+        }
+
+        setCountdownState(newCountdownState)
+
+        await saveToStorage(countdownStorageKey, newCountdownState)
+    }
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            const timestamp = lastCompletedAt
+                ? lastCompletedAt + frequency
+                : Date.now()
+            const isOverdue = isBefore(timestamp, Date.now())
+
+            const distance = intervalToDuration(
+                isOverdue
+                    ? { end: Date.now(), start: timestamp }
+                    : { start: Date.now(), end: timestamp }
+            )
+
+            setStatus({ isOverdue, distance })
+        }, 1000)
+
+        return () => {
+            clearInterval(intervalId)
+        }
+    }, [lastCompletedAt])
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Counter</Text>
+        <View
+            style={[
+                styles.container,
+                status.isOverdue ? styles.containerLate : undefined,
+            ]}
+        >
+            {!status.isOverdue ? (
+                <Text style={[styles.heading]}>Thing due in</Text>
+            ) : (
+                <Text style={[styles.heading, styles.whiteText]}>
+                    Thing overdue by
+                </Text>
+            )}
+            <View style={styles.row}>
+                <TimeSegment
+                    unit="Days"
+                    number={status.distance?.days ?? 0}
+                    textStyle={status.isOverdue ? styles.whiteText : undefined}
+                />
+                <TimeSegment
+                    unit="Hours"
+                    number={status.distance?.hours ?? 0}
+                    textStyle={status.isOverdue ? styles.whiteText : undefined}
+                />
+                <TimeSegment
+                    unit="Minutes"
+                    number={status.distance?.minutes ?? 0}
+                    textStyle={status.isOverdue ? styles.whiteText : undefined}
+                />
+                <TimeSegment
+                    unit="Seconds"
+                    number={status.distance?.seconds ?? 0}
+                    textStyle={status.isOverdue ? styles.whiteText : undefined}
+                />
+            </View>
+            <TouchableOpacity
+                onPress={scheduleNotification}
+                style={styles.button}
+                activeOpacity={0.8}
+            >
+                <Text style={styles.buttonText}>I've done the thing!</Text>
+            </TouchableOpacity>
         </View>
     )
 }
@@ -13,8 +150,36 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        backgroundColor: theme.colorWhite,
     },
     text: {
         fontSize: 24,
+    },
+    row: {
+        flexDirection: 'row',
+        marginBottom: 24,
+    },
+    heading: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 24,
+        color: theme.colorBlack,
+    },
+    containerLate: {
+        backgroundColor: theme.colorRed,
+    },
+    whiteText: {
+        color: theme.colorWhite,
+    },
+    button: {
+        backgroundColor: theme.colorBlack,
+        padding: 12,
+        borderRadius: 6,
+    },
+    buttonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        letterSpacing: 1,
     },
 })
